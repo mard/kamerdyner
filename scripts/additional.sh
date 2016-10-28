98f36c22a222b99f9e2c40e11aaccedfb412c912
Tomasz Kiapsnia <tomasz.kiapsnia@gmail.com>
2016-10-28 16:46:36 +0200

    Fully working version

--- scripts/additional.sh
+++ scripts/additional.sh
@@ -1,32 +1,69 @@
 #!/bin/bash
+#Directory where downloaded mp3 files are stored - kamerdyner's library
+defaultLibraryDir='/media/pen/kamerdyner'
+#Temp dir where downloading and encoding happens
+tmpDir='/media/pen/kamerdyner/tmp'
+
 # Additional functions for downloading and saving files from Youtube
 function checkIfPackageInstalled {
   # usage: checkIfPackageInstalled PackageName
   if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo "Package $1 missing. I will try to install it."
-    sudo apt-get install $1;
+    sudo apt-get install $1 --assume-yes
   else
     echo "Package $1 already installed."
   fi
 }
 
 function downloadYoutube {
-    # usage: downloadYoutube "url" "folder"
+	if [ ! -d "$tmpDir" ]; then
+		mkdir -p "$tmpDir"
+	fi
+	
+    # usage: downloadYoutube "url" "tag"
     outputTemplate="%(title)s.%(ext)s"
-    checkIfPackageInstalled "youtube-dl" >> /dev/null
+    #checkIfPackageInstalled "youtube-dl" ## &> /dev/null
     parameters="--extract-audio --audio-format mp3"
 
     outputFileName=`youtube-dl --get-filename $parameters -o "$outputTemplate" "$1"`
     outputName=`echo "$outputFileName" | cut -d'.' -f1`
-    realOutputFileName="$outputName.mp3"
+    firstOutputFileName="$outputName.mp3"
+    realOutputFileName="$2.mp3"
 
-    pushd $2 >> /dev/null
-      youtube-dl $parameters -o "$outputTemplate" "$1" >> /dev/null
-    popd >> /dev/null
+    pushd $tmpDir &> /dev/null
+      youtube-dl $parameters -o "$outputTemplate" "$1"  &> /dev/null
+      mv "$firstOutputFileName" "$realOutputFileName"  &> /dev/null
+    popd &> /dev/null
 
     echo "$realOutputFileName"
 }
 
+function cutMP3 {
+	if [ -f "$tmpDir/ffmpeg.mp3" ]; then
+		rm "$tmpDir/ffmpeg.mp3"
+ 	fi
+ 	mv "$tmpDir/$1" "$tmpDir/ffmpeg.mp3"
+ 	ffmpeg -y -i "$tmpDir/ffmpeg.mp3" -ss $2 -to $3 -c copy "$defaultLibraryDir/$1" &> /dev/null
+ 	if [ -f "$tmpDir/ffmpeg.mp3" ]; then
+ 		rm "$tmpDir/ffmpeg.mp3"
+ 	fi
+}
+
 # For testing purposes
-m=$(downloadYoutube "https://www.youtube.com/watch?v=QuwvJw1mrWY" "/home/ddi")
-echo "You should have file saved as \"$m\""
+#m=$(downloadYoutube "https://www.youtube.com/watch?v=QuwvJw1mrWY" "BedzieDzialacNIE")
+#echo "You should have file saved as \"$m\""
+#cutMP3 "$defaultLibraryDir/$m" "00:00:01" "00:00:04" "$defaultLibraryDir/outputTAK.mp3"
+
+# Sample usage:
+# ./additional.sh "https://www.youtube.com/watch?v=QuwvJw1mrWY" "BedzieDzialacNIE" "00:00:01" "00:00:04"
+
+if [ "$#" -lt 2 ] || [ "$#" -eq 3 ] || [ "$#" -gt 4 ]; then
+    echo "You should provide 2 or 4 parameters"
+else
+	fileName=`downloadYoutube "$1" "$2"`
+	if [ "$#" -eq 4 ]; then
+		cutMP3 "$fileName" "$3" "$4"
+	else
+		mv "$tmpDir/$fileName" "$defaultLibraryDir/$fileName"
+	fi
+fi
