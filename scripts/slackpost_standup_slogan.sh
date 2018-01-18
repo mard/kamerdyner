08a2d2de3cb09ed33ce796cd070065f1aeef61c1
hackaton team <kip@millwardbrown.com>
2018-01-18 12:10:44 +0100

    Trim CR LF while reading .env

--- scripts/slackpost_standup_slogan.sh
+++ scripts/slackpost_standup_slogan.sh
@@ -10,11 +10,17 @@
 
 standup_slogans_filename=/media/pen/kamerdyner/standup_slogans.txt
 
-while read line; do export $line ; done < ./.env
+while IFS=$'\n\r' read line; do #IFS used to trim out CR or LF from the end of .env 
+	export $line; 
+done < .env
 
+echo "Picking slogan from: $standup_slogans_filename..."
 cat "$standup_slogans_filename" | grep . | sort -R | tail -1 | while read slogan; do
 	escapedText=$(echo $slogan | sed 's/"/\"/g' | sed "s/'/\'/g" )
+	echo "...chosen slogan: $escapedText"
 	json="{\"text\": \"$escapedText\", \"username\": \"von Nogay\"}"
 
+	echo "...message goes to: $HACKATON_SLACK_WEBHOOK_URL"
+        echo "...payload: $json"
 	curl -s -d "payload=$json" "$HACKATON_SLACK_WEBHOOK_URL"
 done
