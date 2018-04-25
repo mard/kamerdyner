0c1c19729bd621d1aa65519a05ac724ced692f3e
Michal Piasecki <michal.piasecki@kantar.com>
2018-04-25 15:39:19 +0200

    Debug var for windows script

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,7 +1,9 @@
 @echo off
 
-call npm install
+rem call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+
+SET HUBOT_LOG_LEVEL=debug
 
 node_modules\.bin\hubot.cmd --name "kamerdyner" %* 
